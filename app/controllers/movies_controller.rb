fba64909dcb283e0e51f74cfd1530b2ba87269e2
maeii426 <mar.anne.369@gmail.com>
2019-02-21 19:59:17 +0000

    25

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,8 +12,7 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    flash[:notice] = params[:order_by]
-    flash[:notice] = session[:order_by]
+    flash[:notice] = params[:order_by], session[:order_by]
     if params[:order_by] != session[:order_by]
       @movies.order!(params[:order_by])
       session[:order_by] = params[:order_by] # HOW?: restrict the possible values of :order_by to only the column names?
