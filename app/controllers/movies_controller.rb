5f663cfbe50450dcae65759ae7be2988113380d6
maeii426 <mar.anne.369@gmail.com>
2019-02-21 20:16:58 +0000

    31

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,10 +15,10 @@ class MoviesController < ApplicationController
     flash[:notice] = params[:order_by], session[:order_by]
     if params[:order_by] != session[:order_by]
       @movies.order!(params[:order_by])
-      @movies.update!()                         # WHEN?: should I save the order to the database??
+      #@movies.update()                         # WHEN?: should I save the order to the database??
       session[:order_by] = params[:order_by]
     elsif params[:order_by] == session[:order_by]
-      @movies.reverse_order!()
+      @movies.order!(params[:order_by]).reverse_order
     end
   end
 
